bfa2d12d52690b93eebd72981f9d645e335f43d2
elihai14 <elihaiafuta@gmail.com>
2025-10-27 16:29:22 +0200

    Add files via upload

---
+++ first.js
@@ -0,0 +1,7 @@
+//daniel elimelech and elihai afuta
+
+const num = 124
+const num2 = num % 2 ==0 ;
+const num3 = num % 3 ==0 ;
+const num5 = num % 5 ==0 ;
+console.log(Number(num2)+ Number(num3) + Number(num5));
